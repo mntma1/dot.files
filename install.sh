8b8e8e2985944b71f5798bd2b053da3b0ff3fa05
Manfred N <114916491+mntma1@users.noreply.github.com>
2025-07-01 14:16:07 +0200

    Update install.sh by Manfred

--- install.sh
+++ install.sh
@@ -1,13 +1,22 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 # Crerated: by Manfred 
 # Date:01.07.2025
 #
-cp alias-mafred ~/.alias
-cp bashrc-manfred ~/.bashrc
-cp functions ~/.functions
-cp tmux.conf ~/.tmux.conf
-tar xvzf dot.tmux.tgz 
-cp dot.tmux-session -/.tmux-sessionr
-sudo cp alias-root /root/.alias 
-sudo install color /usr/local/bin
-exit 00
+sudo usermod -aG sudo $USER
+sudo apt install git tmux
+mkdir ~/conf && cd ~/conf
+tar xvzf dot.tmux.tgz
+cd dot.tmux
+cp -av dot.tmux ~/.tmux
+cp -v alias-mafred ~/.alias
+cp -fv bashrc-manfred ~/.bashrc
+cp -v functions ~/.functions
+cp -v tmux.conf ~/.tmux.conf
+cp -v dot.tmux-session -/.tmux-session
+#sudo cp -v functions ~/root/.functions
+#sudo cp -v alias-root /root/.alias
+#sudo cp -v dot.tmux-session -/.tmux-sessionr
+#sudo cp -v tmux.conf ~/root/.tmux.conf
+#sudo install color /usr/local/bin
+source ~/.bashrc
+exit 0
