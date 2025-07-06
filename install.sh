33b4b493deaa49d1c9bcc60818263dcdb70bf43e
Manfred N <114916491+mntma1@users.noreply.github.com>
2025-07-06 22:09:01 +0200

    Geändert von Manfred

--- install.sh
+++ install.sh
@@ -1,24 +1,42 @@
 #!/usr/bin/bash
 # Crerated: by Manfred 
-# Date:01.07.2025
-#
+# Date: 06.07.2025
+
+echo "$(color green)Gib hier dein Passwort ein!$(color)"
+sudo install color /usr/local/bin
 tar xvzf dot.tmux.tgz
 cp -av dot.tmux ~/.tmux
-cp -fv alias-mafred ~/.alias
-cp -fv bashrc-manfred ~/.bashrc
+cp -fv alias ~/.alias
+cp -fv bashrc ~/.bashrc
 cp -fv functions ~/.functions
 cp -fv dot.screenrc ~/.screenrc
 cp -fv tmux.conf ~/.tmux.conf
 cp -fv dot.tmux-session ~/.tmux-session
-#sudo cp -v functions /root/.functions
-#sudo cp -v alias-root /root/.alias
-#sudo cp -v dot.tmux-session /root/.tmux-sessionr
-#sudo cp -v tmux.conf ~/root/.tmux.conf
-#sudo install color /usr/local/bin
 source ~/.bashrc
 cd ~
-echo
-echo ....fertig
-echo 
+cat<<EOF
+sleep 5
+$(color white red)....Bitte warten$(color)
+EOF
+#echo "$(color green)Gib hier dein Passwort ein!$(color)"
+#sudo install tmux -y
 #tmux
+clear;cat<<EOF
+Du bist User: $(color white blue)$USER§(color)
+
+Wenn "root" auch in den Genuß dieiser Umgebung kommen soll,
+dann feuer folgende Befehle ab:
+
+ sudo cp -av dot.tmux /root/.tmux
+ sudo cp -fv functions /root/.functions
+ sudo cp -fv alias-root /root/.alias
+ sudo cp -fv dot.tmux-session /root/.tmux-session
+ sudo cp -fv tmux.conf ~/root/.tmux.conf
+ sudo install tmux -y
+
+Viel Spaß
+Gruß Manfred
+EOF
+echo
+echo "$(color cyan)Alles erledigt!$(color)"
 exit 0
