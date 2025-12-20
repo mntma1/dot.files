9fad80d0eb8d436974b308aacadd397372105cdd
Manfred N <114916491+mntma1@users.noreply.github.com>
2025-12-20 08:40:01 +0100

    Hinzugefügt: apt install vim kate xrdp net-tools dnsutils htop mc ccze gdebi ncdu -y

--- install-root.sh
+++ install-root.sh
@@ -2,6 +2,8 @@
 # Crerated: by Manfred 
 # Date: 06.07.2025
 
+clear;
+apt install vim kate xrdp net-tools dnsutils htop mc ccze gdebi ncdu -y
 install color /usr/local/bin
 tar xvzf dot.tmux.tgz
 cp -av dot.tmux ~/.tmux
@@ -12,7 +14,6 @@ cp -fv dot.screenrc ~/.screenrc
 cp -fv tmux.conf ~/.tmux.conf
 cp -fv dot.tmux-session ~/.tmux-session
 
-#apt install vim net-tools dnsutils mc htop ccze ncdu tmux screen -y
 
 sleep 3
 clear
